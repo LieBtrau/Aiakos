dedd9c142581ce47b12dae2f383ffe5c8e9a5dee
Christoph Tack <prog.send@gmail.com>
2015-09-15 21:07:18 +0200

    Enabling NFC and wireless communication simultaneously.
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- Nucleo/file1.cpp
+++ Nucleo/file1.cpp
@@ -30,15 +30,15 @@ RH_MRF89XA driver(3, 5, 4, 2);
 
 // Class to manage message delivery and receipt, using the driver declared above
 #if defined(CLIENT_MRF89XA_RELIABLE)
-    #ifdef SERVER_MRF89XA_RELIABLE
-        #error You have to choose: client OR server, not both
-    #endif
-    RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+#ifdef SERVER_MRF89XA_RELIABLE
+#error You have to choose: client OR server, not both
+#endif
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
 #elif defined(SERVER_MRF89XA_RELIABLE)
-    #ifdef CLIENT_MRF89XA_RELIABLE
-        #error You have to choose: client OR server, not both
-    #endif
-    RHReliableDatagram manager(driver, SERVER_ADDRESS);
+#ifdef CLIENT_MRF89XA_RELIABLE
+#error You have to choose: client OR server, not both
+#endif
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
 #endif
 
 uint8_t data[] = "Hello World!";
@@ -54,13 +54,16 @@ uint8_t buf[RH_MRF89XA_MAX_MESSAGE_LEN];
 //SS => D6
 //VCC => 3V3 (or 5V)
 //GND => ICSP.6
-//PN532_SPI pn532spi(SPI, 6);
-//NfcAdapter nfc = NfcAdapter(pn532spi);
+PN532_SPI pn532spi(SPI, 6);
+NfcAdapter nfc = NfcAdapter(pn532spi);
+uint32_t ulStartTime;
+uint32_t ulStartTime2;
 
 void setup() {
+    ulStartTime2=ulStartTime=millis();
     Serial.begin(115200);
     Serial.println("start");
-//    nfc.begin();
+    nfc.begin();
 #if defined(CLIENT_MRF89XA_RELIABLE) || defined(SERVER_MRF89XA_RELIABLE)
     if (!manager.init()){
         Serial.println("manager init failed");
@@ -76,106 +79,110 @@ void setup() {
 #endif
 }
 
+
 void loop() {
-//    Serial.println("\nScan a NFC tag\n");
-//    if (nfc.tagPresent())
-//    {
-//        NfcTag tag = nfc.read();
-//        tag.print(&Serial);
-//    }
-//    delay(5000);
-
-#ifdef CLIENT_MRF89XA_RELIABLE
-    Serial.println("Sending to mrf89xa_reliable_datagram_server");
-
-    // Send a message to manager_server
-    if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
-    {
-        // Now wait for a reply from the server
-        uint8_t len = sizeof(buf);
-        uint8_t from;
-        if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    if(millis()>ulStartTime+5000){
+        ulStartTime=millis();
+        Serial.println("\nScan a NFC tag\n");
+        if (nfc.tagPresent())
         {
-            Serial.print("got reply from : 0x");
-            Serial.print(from, HEX);
-            Serial.print(": ");
-            Serial.println((char*)buf);
+            NfcTag tag = nfc.read();
+            tag.print(&Serial);
+        }
+    }
+    if(millis()>ulStartTime2+3000){
+        ulStartTime2=millis();
+#ifdef CLIENT_MRF89XA_RELIABLE
+        Serial.println("Sending to mrf89xa_reliable_datagram_server");
+
+        // Send a message to manager_server
+        if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
+        {
+            // Now wait for a reply from the server
+            uint8_t len = sizeof(buf);
+            uint8_t from;
+            if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+            {
+                Serial.print("got reply from : 0x");
+                Serial.print(from, HEX);
+                Serial.print(": ");
+                Serial.println((char*)buf);
+            }
+            else
+            {
+                Serial.println("No reply, is mrf89xa_reliable_datagram_server running?");
+            }
+        }
+
+#elif defined(SERVER_MRF89XA_RELIABLE)
+        if (manager.available())
+        {
+            // Wait for a message addressed to us from the client
+            uint8_t len = sizeof(buf);
+            uint8_t from;
+            if (manager.recvfromAck(buf, &len, &from))
+            {
+                Serial.print("got request from : 0x");
+                Serial.print(from, HEX);
+                Serial.print(": ");
+                Serial.println((char*)buf);
+
+                // Send a reply back to the originator client
+                if (!manager.sendtoWait(data, sizeof(data), from))
+                    Serial.println("sendtoWait failed");
+            }else{
+                Serial.println("not valid");
+            }
+        }
+#elif defined(CLIENT_MRF89XA_SIMPLE)
+        Serial.println("Sending to mrf89xa");
+        // Send a message to mrf89xa
+        driver.send(data, sizeof(data));
+
+        driver.waitPacketSent();
+        // Now wait for a reply
+        uint8_t len = sizeof(buf);
+
+        if (driver.waitAvailableTimeout(500))
+        {
+            // Should be a reply message for us now
+            if (driver.recv(buf, &len))
+            {
+                Serial.print("got reply: ");
+                Serial.println((char*)buf);
+            }
+            else
+            {
+                Serial.println("recv failed");
+            }
         }
         else
         {
-            Serial.println("No reply, is mrf89xa_reliable_datagram_server running?");
+            Serial.println("No reply, is mrf89xa_server running?");
         }
-    }
-
-#elif defined(SERVER_MRF89XA_RELIABLE)
-    if (manager.available())
-    {
-        // Wait for a message addressed to us from the client
-        uint8_t len = sizeof(buf);
-        uint8_t from;
-        if (manager.recvfromAck(buf, &len, &from))
-        {
-            Serial.print("got request from : 0x");
-            Serial.print(from, HEX);
-            Serial.print(": ");
-            Serial.println((char*)buf);
-
-            // Send a reply back to the originator client
-            if (!manager.sendtoWait(data, sizeof(data), from))
-                Serial.println("sendtoWait failed");
-        }else{
-            Serial.println("not valid");
-        }
-    }
-#elif defined(CLIENT_MRF89XA_SIMPLE)
-    Serial.println("Sending to mrf89xa");
-    // Send a message to mrf89xa
-    driver.send(data, sizeof(data));
-
-    driver.waitPacketSent();
-    // Now wait for a reply
-    uint8_t len = sizeof(buf);
-
-    if (driver.waitAvailableTimeout(500))
-    {
-      // Should be a reply message for us now
-      if (driver.recv(buf, &len))
-      {
-        Serial.print("got reply: ");
-        Serial.println((char*)buf);
-      }
-      else
-      {
-        Serial.println("recv failed");
-      }
-    }
-    else
-    {
-      Serial.println("No reply, is mrf89xa_server running?");
-    }
-    delay(400);
+        delay(400);
 #elif defined(SERVER_MRF89XA_SIMPLE)
-    if (driver.available())
-    {
-      // Should be a message for us now
-      uint8_t len = sizeof(buf);
-      if (driver.recv(buf, &len))
-      {
-  //      NRF24::printBuffer("request: ", buf, len);
-        Serial.print("got request: ");
-        Serial.println((char*)buf);
+        if (driver.available())
+        {
+            // Should be a message for us now
+            uint8_t len = sizeof(buf);
+            if (driver.recv(buf, &len))
+            {
+                //      NRF24::printBuffer("request: ", buf, len);
+                Serial.print("got request: ");
+                Serial.println((char*)buf);
 
-        // Send a reply
-        uint8_t data[] = "And hello back to you";
-        driver.send(data, sizeof(data));
-        driver.waitPacketSent();
-        Serial.println("Sent a reply");
-      }
-      else
-      {
-        Serial.println("recv failed");
-      }
-    }
-
+                // Send a reply
+                uint8_t data[] = "And hello back to you";
+                driver.send(data, sizeof(data));
+                driver.waitPacketSent();
+                Serial.println("Sent a reply");
+            }
+            else
+            {
+                Serial.println("recv failed");
+            }
+        }
 #endif
+    }
 }
