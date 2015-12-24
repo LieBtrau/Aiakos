855e55bc70f1bfee9d54822821675ffe6847d6cf
Christoph Tack <prog.send@gmail.com>
2015-12-24 16:29:42 +0100

    Working application of reader and tag sending messages to each other (running on one MCU)
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- Nucleo/file1.cpp
+++ Nucleo/file1.cpp
@@ -11,8 +11,8 @@
 
 char historyBuf[100];
 char hostname[] = "ioBash";
-char privateKey[32+1];//base64 representation of 192bit key
-char publicKey[64+1];//base64 representation of 192bit key (x,y)
+char privateKey[32+1];//base64 string representation of 192bit key
+char publicKey[64+1];//base64 string representation of 192bit key (x,y)
 
 PARAM_ENTRY Params[]=
 {
@@ -73,9 +73,10 @@ Ntag ntag(Ntag::NTAG_I2C_1K);
 NtagSramAdapter ntagAdapter(&ntag);
 uint32_t ulStartTime;
 uint32_t ulStartTime2;
-byte ndefdata[30];
 
 void i2cRelease();
+void readerLoop();
+void tagLoop(bool bInitialize=false);
 
 void setup() {
     ulStartTime2=ulStartTime=millis();
@@ -84,23 +85,7 @@ void setup() {
     i2cRelease();
     nfc.begin();
     ntagAdapter.begin();
-    NdefMessage message = NdefMessage();
-    message.addUnknownRecord(data,sizeof(data));
-    if(ntagAdapter.write(message)){
-        Serial.println("Message written to tag.");
-    }
-    NfcTag nf=ntagAdapter.read();
-    if(nf.hasNdefMessage()){
-        NdefMessage nfm=nf.getNdefMessage();
-        nfm.print();
-        NdefRecord ndf=nfm.getRecord(0);
-        byte dat[ndf.getPayloadLength()];
-        ndf.getPayload(dat);
-        for(byte i=0;i<ndf.getPayloadLength();i++){
-            Serial.print(dat[i], HEX);Serial.print(" ");
-        }
-        Serial.println();
-    }
+    tagLoop(true);
     //    bool bResult=cryptop.testMasterKeySse();
     //    Serial.print("Test master key Agreement + Derivation + Confirmation: ");
     //    Serial.println(bResult?"OK":"Fail");
@@ -127,15 +112,8 @@ void setup() {
 
 void loop() {
     //    microbox.cmdParser();
-    if(nfc.tagPresent()){
-        NfcTag tag = nfc.read();
-        tag.print();
-        delay(5000);
-    }
-    ntag.debug();
-    if(ntag.waitUntilNdefRead(5000)){
-        Serial.println("Tag has been read");
-    }
+    readerLoop();
+    tagLoop();
     //    if(millis()>ulStartTime2+3000){
     //        ulStartTime2=millis();
     //        cryptop.eccTest();
@@ -235,6 +213,65 @@ void loop() {
 #endif
     }
     */
+}
+
+void tagLoop(bool bInitialize){
+    byte id=0;
+    if(!bInitialize){
+        if(!ntag.waitUntilNdefRead(10)){
+            return;
+        }
+        NfcTag nf=ntagAdapter.read();
+        if(!nf.hasNdefMessage()){
+            return;
+        }
+        NdefMessage nfm=nf.getNdefMessage();
+        nfm.print();
+        NdefRecord ndf=nfm.getRecord(0);
+        byte dat[ndf.getPayloadLength()];
+        ndf.getPayload(dat);
+        for(byte i=0;i<ndf.getPayloadLength();i++){
+            Serial.print(dat[i], HEX);Serial.print(" ");
+        }
+        Serial.println();
+        id=dat[0];
+    }
+    if(bitRead(id,0)==0){
+        data[0]=id+1;
+        NdefMessage message = NdefMessage();
+        message.addUnknownRecord(data,sizeof(data));
+        if(ntagAdapter.write(message)){
+            Serial.println("Tag has written message written to tag.");
+        }
+    }
+}
+
+void readerLoop()
+{
+    if(!nfc.tagPresent()){
+        return;
+    }
+    NfcTag tag = nfc.read();
+    if(!tag.hasNdefMessage()){
+        return;
+    }
+    NdefMessage nfm=tag.getNdefMessage();
+    nfm.print();
+    NdefRecord ndf=nfm.getRecord(0);
+    byte dat[ndf.getPayloadLength()];
+    ndf.getPayload(dat);
+    for(byte i=0;i<ndf.getPayloadLength();i++){
+        Serial.print(dat[i], HEX);Serial.print(" ");
+    }
+    Serial.println();
+    if(bitRead(dat[0],0)){
+        NdefMessage message = NdefMessage();
+        data[0]=dat[0]+1;
+        message.addUnknownRecord(data,sizeof(data));
+        if(nfc.write(message)){
+            Serial.println("Reader has written message to tag.");
+        }
+    }
 }
 
 void i2cRelease()
