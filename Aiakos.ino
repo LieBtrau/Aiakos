743f454240c3cebfd27afc9c9bacd5ed46ff963f
Christoph Tack <prog.send@gmail.com>
2017-01-28 22:39:26 +0100

    Adding wired serial port support
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- Aiakos.ino
+++ Aiakos.ino
@@ -1,5 +1,7 @@
 #include <RHReliableDatagram.h>
 #include <RH_RF95.h>
+#include <RH_Serial.h>
+#include <RH_SwSerial.h>
 #include <SPI.h>
 
 /* Connections to Nucleo F103RB
@@ -10,26 +12,31 @@
 * MISO     12               D12
 * SCK      13               D13  (Be careful, this is the 6th pin on the left row of the right most pin header connector, not the fifth!)
 * REST     RST              NRST
-* DIO0     3                D2
+* DIO0     3                D3
 * VCC      3V               3V
 * GND      G                GND
 *
 */
-#define SERVER
 
 const byte CLIENT_ADDRESS=1;
 const byte SERVER_ADDRESS=2;
 
-#ifdef SERVER && !defined(CLIENT)
+#ifdef ARDUINO_STM_NUCLEO_F103RB
 //Nucleo
-RH_RF95 driver(A2,2);
 uint8_t data[] = "And hello back to you";
+RH_RF95 driver(A2,3);
 RHReliableDatagram manager(driver, SERVER_ADDRESS);
-#elif defined(CLIENT) && !defined(SERVER)
+RH_Serial driverSerial(Serial1);
+RHReliableDatagram managerSerial(driverSerial, SERVER_ADDRESS);
+#elif defined(ARDUINO_AVR_PROTRINKET3)
 //ProTrinket
 uint8_t data[] = "Hello World!";
 RH_RF95 driver(10,3);
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+#include <SoftwareSerial.h>
+SoftwareSerial SerialSw(4, 5); // RX, TX
+RH_SwSerial driverSerial(SerialSw);
+RHReliableDatagram managerSerial(driverSerial, CLIENT_ADDRESS);
 #else
 #error No device type defined.
 #endif
@@ -42,6 +49,11 @@ void setup()
     {
         Serial.println("init failed");
     }
+    driverSerial.serial().begin(9600);
+    if (!managerSerial.init())
+    {
+        Serial.println("init serial failed");
+    }
 }
 
 // Dont put this on the stack:
@@ -49,48 +61,48 @@ uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
 
 void loop()
 {
-#ifdef SERVER
-    if (manager.available())
+#ifdef ARDUINO_STM_NUCLEO_F103RB
+    if (managerSerial.available())
     {
-      // Wait for a message addressed to us from the client
-      uint8_t len = sizeof(buf);
-      uint8_t from;
-      if (manager.recvfromAck(buf, &len, &from))
-      {
-        Serial.print("got request from : 0x");
-        Serial.print(from, HEX);
-        Serial.print(": ");
-        Serial.println((char*)buf);
+        // Wait for a message addressed to us from the client
+        uint8_t len = sizeof(buf);
+        uint8_t from;
+        if (managerSerial.recvfromAck(buf, &len, &from))
+        {
+            Serial.print("got request from : 0x");
+            Serial.print(from, HEX);
+            Serial.print(": ");
+            Serial.println((char*)buf);
 
-        // Send a reply back to the originator client
-        if (!manager.sendtoWait(data, sizeof(data), from))
-          Serial.println("sendtoWait failed");
-      }
+            // Send a reply back to the originator client
+            if (!managerSerial.sendtoWait(data, sizeof(data), from))
+                Serial.println("sendtoWait failed");
+        }
     }
-#elif defined(CLIENT)
-  Serial.println("Sending to rf95_reliable_datagram_server");
+#elif defined(ARDUINO_AVR_PROTRINKET3)
+    Serial.println("Sending to rf95_reliable_datagram_server");
 
-  // Send a message to manager_server
-  if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
-  {
-    // Now wait for a reply from the server
-    uint8_t len = sizeof(buf);
-    uint8_t from;
-    if (manager.recvfromAckTimeout(buf, &len, 2000, &from))
+    // Send a message to manager_server
+    if (managerSerial.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
     {
-      Serial.print("got reply from : 0x");
-      Serial.print(from, HEX);
-      Serial.print(": ");
-      Serial.println((char*)buf);
+        // Now wait for a reply from the server
+        uint8_t len = sizeof(buf);
+        uint8_t from;
+        if (managerSerial.recvfromAckTimeout(buf, &len, 2000, &from))
+        {
+            Serial.print("got reply from : 0x");
+            Serial.print(from, HEX);
+            Serial.print(": ");
+            Serial.println((char*)buf);
+        }
+        else
+        {
+            Serial.println("No reply, is rf95_reliable_datagram_server running?");
+        }
     }
     else
-    {
-      Serial.println("No reply, is rf95_reliable_datagram_server running?");
-    }
-  }
-  else
-    Serial.println("sendtoWait failed");
-  delay(500);
+        Serial.println("sendtoWait failed");
+    delay(500);
 #endif
 }
 
