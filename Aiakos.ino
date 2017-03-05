1506c9903fda75f1a6f7f4fb93fd801d0181a323
Christoph Tack <prog.send@gmail.com>
2017-03-05 21:10:01 +0100

    Secure pairing (wired) and authentication (wireless) are both working
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- Aiakos.ino
+++ Aiakos.ino
@@ -15,10 +15,6 @@ void dataReceived(byte* data, byte length)
 }
 
 const byte IDLENGTH=9;
-byte sharedkey[16]={0xA,0xB,0xC,0xD,
-                    0xE,0xF,0xE,0xD,
-                    0xC,0xB,0xA,0x9,
-                    0x8,0x7,0x6,0x5};
 byte id2[]={9,8,7,6,5,4,3,2,1};
 
 /* Connections to Nucleo F103RB
@@ -29,7 +25,7 @@ byte id2[]={9,8,7,6,5,4,3,2,1};
 * MISO     12               ICSP.1  D12
 * SCK      13               ICSP.3  D13  (Be careful, this is the 6th pin on the left row of the right most pin header connector, not the fifth!)
 * REST     RST              RESET   NRST
-* DIO0     3                3       D2
+* DIO0     3                3       D5
 * VCC      3V               3.3V    3V
 * GND      G                GND     GND
 *                           RX1     D8
@@ -41,10 +37,10 @@ const byte ADDRESS2=2;
 #ifdef ARDUINO_STM_NUCLEO_F103RB
 //STM Nucleo = Garage controller
 byte payload[4]={0xFE, 0xDC, 0xBA, 0x98};
-//Kryptoknight k1= Kryptoknight(sharedkey,&ATSHA_RNG, writeDataLoRa, readDataLoRa);
+Kryptoknight k1= Kryptoknight(&ATSHA_RNG, writeDataLoRa, readDataLoRa);
 EcdhComm ecdh1= EcdhComm(&ATSHA_RNG, writeDataSer, readDataSer);
-//RH_RF95 rhLoRa(A2,5);//NSS, DIO0
-//RHReliableDatagram mgrLoRa(rhLoRa, ADDRESS1);
+RH_RF95 rhLoRa(A2,5);//NSS, DIO0
+RHReliableDatagram mgrLoRa(rhLoRa, ADDRESS1);
 RH_Serial rhSerial(Serial1);
 RHReliableDatagram mgrSer(rhSerial, ADDRESS1);
 ATCAIfaceCfg *gCfg = &cfg_sha204a_i2c_default;
@@ -56,10 +52,10 @@ ATCAIfaceCfg *gCfg = &cfg_sha204a_i2c_default;
 //RHReliableDatagram manager(driver, ADDRESS2);
 #elif defined(ARDUINO_SAM_DUE)
 //Arduino Due = Key fob
-//Kryptoknight k2= Kryptoknight(id2,IDLENGTH,sharedkey, &RNG, writeDataLoRa, readDataLoRa);
+Kryptoknight k2= Kryptoknight(id2, IDLENGTH, &RNG, writeDataLoRa, readDataLoRa);
 EcdhComm ecdh2=EcdhComm(&RNG, writeDataSer, readDataSer);
-//RH_RF95 rhLoRa(4,3);
-//RHReliableDatagram mgrLoRa(rhLoRa, ADDRESS2);
+RH_RF95 rhLoRa(4,3);
+RHReliableDatagram mgrLoRa(rhLoRa, ADDRESS2);
 RH_Serial rhSerial(Serial1);
 RHReliableDatagram mgrSer(rhSerial, ADDRESS2);
 #else
@@ -71,13 +67,13 @@ void setup()
     Serial.begin(9600);
     rhSerial.serial().begin(9600);
     while (!Serial) ; // Wait for serial port to be available
-//    if ((!mgrLoRa.init()))
-//    {
-//#ifdef DEBUG
-//        Serial.println("LoRa init failed");
-//#endif
-//        return;
-//    }
+    if ((!mgrLoRa.init()))
+    {
+#ifdef DEBUG
+        Serial.println("LoRa init failed");
+#endif
+        return;
+    }
     if ((!mgrSer.init()))
     {
 #ifdef DEBUG
@@ -87,26 +83,21 @@ void setup()
     }
 #ifdef ARDUINO_STM_NUCLEO_F103RB
     byte buf[10];
-    if((!getSerialNumber(buf,IDLENGTH)) /*|| (!k1.setLocalId(buf,IDLENGTH) )*/ || (!ecdh1.init(buf, IDLENGTH)) )
+    if((!getSerialNumber(buf,IDLENGTH)) || (!k1.setLocalId(buf,IDLENGTH) ) || (!ecdh1.init(buf, IDLENGTH)) )
     {
        return;
     }
     if(!ecdh1.startPairing())
     {
         Serial.println("Sending message failed.");
+        return;
     }
-//    Serial.println("Initiator starts authentication");
-//    if(!k1.sendMessage(id2,payload,4))
-//    {
-//        Serial.println("Sending message failed.");
-//        return;
-//    }
 #elif defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_SAM_DUE)
     if(!ecdh2.init(id2, IDLENGTH))
     {
         return;
     }
-    //k2.setMessageReceivedHandler(dataReceived);
+    k2.setMessageReceivedHandler(dataReceived);
 #else
 #error No device
 #endif
@@ -120,22 +111,30 @@ void setup()
 void loop()
 {
 #ifdef ARDUINO_STM_NUCLEO_F103RB
-//    if(k1.loop()==Kryptoknight::AUTHENTICATION_AS_INITIATOR_OK)
-//    {
-//        Serial.println("Message received by peer and acknowledged");
-//    }
-    if(ecdh1.loop()==EcdhComm::AUTHENTICATION_OK)
+    if(k1.loop()==Kryptoknight::AUTHENTICATION_AS_INITIATOR_OK)
     {
         Serial.println("Message received by peer and acknowledged");
     }
+    if(ecdh1.loop()==EcdhComm::AUTHENTICATION_OK)
+    {
+        Serial.println("Securely paired");
+        k1.setSharedKey(ecdh1.getMasterKey());
+        Serial.println("Initiator starts authentication");
+        if(!k1.sendMessage(id2,payload,4))
+        {
+            Serial.println("Sending message failed.");
+            return;
+        }
+    }
 #elif defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_SAM_DUE)
-//    if(k2.loop()==Kryptoknight::AUTHENTICATION_AS_PEER_OK)
-//    {
-//        Serial.println("Message received by remote initiator");
-//    }
+    if(k2.loop()==Kryptoknight::AUTHENTICATION_AS_PEER_OK)
+    {
+        Serial.println("Message received by remote initiator");
+    }
     if(ecdh2.loop()==EcdhComm::AUTHENTICATION_OK)
     {
-        Serial.println("Message received by peer and acknowledged");
+        Serial.println("Securely paired");
+        k2.setSharedKey(ecdh2.getMasterKey());
     }
 #else
 #error No device
@@ -156,19 +155,19 @@ bool writeDataSer(byte* data, byte length)
 #endif
 }
 
-//bool writeDataLoRa(byte* data, byte length)
-//{
-//#ifdef DEBUG
-//        Serial.println("Sending data: ");print(data, length);
-//#endif
-//#ifdef ARDUINO_STM_NUCLEO_F103RB
-//    return mgrLoRa.sendtoWait(data, length, ADDRESS2);
-//#elif defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_SAM_DUE)
-//    return mgrLoRa.sendtoWait(data, length, ADDRESS1);
-//#else
-//#error No device
-//#endif
-//}
+bool writeDataLoRa(byte* data, byte length)
+{
+#ifdef DEBUG
+        Serial.println("Sending data: ");print(data, length);
+#endif
+#ifdef ARDUINO_STM_NUCLEO_F103RB
+    return mgrLoRa.sendtoWait(data, length, ADDRESS2);
+#elif defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_SAM_DUE)
+    return mgrLoRa.sendtoWait(data, length, ADDRESS1);
+#else
+#error No device
+#endif
+}
 
 bool readDataSer(byte** data, byte& length)
 {
@@ -203,38 +202,38 @@ bool readDataSer(byte** data, byte& length)
     return true;
 }
 
-//bool readDataLoRa(byte** data, byte& length)
-//{
-//    byte from;
-//    if (!mgrLoRa.available())
-//    {
-//        return false;
-//    }
-//    if(!mgrLoRa.recvfromAck(*data, &length, &from))
-//    {
-//        return false;
-//    }
-//#ifdef ARDUINO_STM_NUCLEO_F103RB
-//    if(from != ADDRESS2)
-//    {
-//#ifdef DEBUG
-//        Serial.println("Sender doesn't match");
-//#endif
-//        return false;
-//    }
-//#elif defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_SAM_DUE)
-//    if(from != ADDRESS1)
-//    {
-//#ifdef DEBUG
-//        Serial.println("Sender doesn't match");
-//#endif
-//        return false;
-//    }
-//#else
-//#error No device
-//#endif
-//    return true;
-//}
+bool readDataLoRa(byte** data, byte& length)
+{
+    byte from;
+    if (!mgrLoRa.available())
+    {
+        return false;
+    }
+    if(!mgrLoRa.recvfromAck(*data, &length, &from))
+    {
+        return false;
+    }
+#ifdef ARDUINO_STM_NUCLEO_F103RB
+    if(from != ADDRESS2)
+    {
+#ifdef DEBUG
+        Serial.println("Sender doesn't match");
+#endif
+        return false;
+    }
+#elif defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_SAM_DUE)
+    if(from != ADDRESS1)
+    {
+#ifdef DEBUG
+        Serial.println("Sender doesn't match");
+#endif
+        return false;
+    }
+#else
+#error No device
+#endif
+    return true;
+}
 
 
 #ifdef ARDUINO_STM_NUCLEO_F103RB
